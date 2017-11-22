1617391ac28c46e6d8f303ff00b1de729194c2ad
Unknown <zephyrmahe@msn.com>
2017-11-22 11:48:55 +0100

    modification mineur de l'affichage du script

--- javascript/script.js
+++ javascript/script.js
@@ -3,9 +3,9 @@ var valeur = 1;
 var valRad = 1;
 function ajoutQuestion() {
 
-	$('#addQuest').append("<div id='val"+valeur+"'><input type='text' placeholder='Votre question' id='question"+valeur+"'><br>"
+	$('#addQuest').append("<br><div id='val"+valeur+"'><input type='text' placeholder='Votre question' id='question"+valeur+"'><br>"
 	 + "<button class='btn btn-outline-primary' onclick=\"ajoutRadio('#val"+valeur+"', "+valeur+");\">ajouter une option</button>"
-	 + "<button class='btn btn-outline-danger' onclick='suppQuestion(val"+valeur+");'>supprimer cette question</button><br><br></div>");
+	 + "<button class='btn btn-outline-danger' onclick='suppQuestion(val"+valeur+");'>supprimer cette question</button><br></div>");
 	
 	valeur++;
 
@@ -14,7 +14,10 @@ function ajoutQuestion() {
 function ajoutRadio(elem, radId){
 		$(elem)
 		.append(
-			'<div id="option'+valRad+'"><input type="radio" class="onTouchePas" name="rad'+radId+'"><input type="text" placeholder="choix de reponse"><button class="btn btn-danger" alt="supprimer cette option" onclick="suppOption(option'+valRad+');">x</button><br></div>'
+			'<div id="option'+valRad+'"><input type="radio" class="onTouchePas" name="rad'+radId+'">'
+			+ '<input type="text" placeholder="choix de reponse">'
+			+ '<button class="btn btn-danger" title="supprimer cette option" onclick="suppOption(option'+valRad+');">'
+			+'supprimer l\'option</button><br></div>'
 			);
 		valRad++;
 	}
