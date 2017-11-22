bb889653cd0ca6f1fbf527edccecaed41601a706
Unknown <zephyrmahe@msn.com>
2017-11-22 14:09:30 +0100

    ajout d'un choix multiple

--- javascript/script.js
+++ javascript/script.js
@@ -3,9 +3,11 @@ var valeur = 1;
 var valRad = 1;
 function ajoutQuestion() {
 
-	$('#addQuest').append(
+	$('#addQuest')
+	.append(
 		"<div id='val"+valeur+"'><br><input type='text' placeholder='Votre question' id='question"+valeur+"'><br>"
 	 	+ "<button class='btn btn-outline-primary' onclick=\"ajoutRadio('#val"+valeur+"', "+valeur+");\">ajouter une option</button>"
+	 	+ "<button class='btn btn-outline-secondary' onclick=\"ajoutCheck('#val"+valeur+"', "+valeur+");\">ajouter un choix multiple</button>"
 	 	+ "<button class='btn btn-outline-danger' title='supprimer cette question' onclick='suppQuestion(val"+valeur+");'>"
 	 	+"supprimer cette question</button><br></div>"
 	 );
@@ -18,6 +20,17 @@ function ajoutRadio(elem, radId){
 		$(elem)
 		.append(
 			'<div id="option'+valRad+'"><input type="radio" class="onTouchePas" name="rad'+radId+'">'
+			+ '<input type="text" placeholder="choix de reponse">'
+			+ '<button class="btn btn-danger" title="supprimer cette option" onclick="suppOption(option'+valRad+');">'
+			+'supprimer l\'option</button><br></div>'
+			);
+		valRad++;
+	}
+
+	function ajoutCheck(elem, radId){
+		$(elem)
+		.append(
+			'<div id="option'+valRad+'"><input type="checkbox" class="onTouchePas" name="rad'+radId+'">'
 			+ '<input type="text" placeholder="choix de reponse">'
 			+ '<button class="btn btn-danger" title="supprimer cette option" onclick="suppOption(option'+valRad+');">'
 			+'supprimer l\'option</button><br></div>'
